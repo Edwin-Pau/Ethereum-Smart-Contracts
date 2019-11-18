4c9e0fb18ae830e672389392632d528e94a93544
zillirammy <pau.edwin@live.ca>
2019-11-18 04:24:14 -0800

    added next.js setup

---
+++ fundraiser-react-webapp/.next/dist/pages/newcampaign.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'C:\\Users\\miner01\\Desktop\\ethereum-smart-contracts\\fundraiser-react-webapp\\pages\\newcampaign.js?entry';
+
+exports.default = function () {
+    return _react2.default.createElement('h1', {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 5
+        }
+    }, 'This is the new campaign page!');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxuZXdjYW1wYWlnbi5qcyJdLCJuYW1lcyI6WyJSZWFjdCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTyxBQUVQOzs7Ozs7OztrQkFBZSxZQUFNLEFBQ2pCOzJCQUNJLGNBQUE7O3NCQUFBO3dCQUFBO0FBQUE7QUFBQSxLQUFBLEVBREosQUFDSSxBQUVQO0FBSkQiLCJmaWxlIjoibmV3Y2FtcGFpZ24uanM/ZW50cnkiLCJzb3VyY2VSb290IjoiQzovVXNlcnMvbWluZXIwMS9EZXNrdG9wL2V0aGVyZXVtLXNtYXJ0LWNvbnRyYWN0cy9mdW5kcmFpc2VyLXJlYWN0LXdlYmFwcCJ9
