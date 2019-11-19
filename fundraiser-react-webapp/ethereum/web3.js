b9a77ba189a63f4eccab2ce9c03cd8ea8472e543
zillirammy <pau.edwin@live.ca>
2019-11-19 04:44:23 -0800

    Next.js

--- fundraiser-react-webapp/ethereum/web3.js
+++ fundraiser-react-webapp/ethereum/web3.js
@@ -1,6 +1,19 @@
 import Web3 from 'web3';
 
-// Instance of the web3 using injected metamask
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+// Conditionals to see if we are running on the server or browser.
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // This is for the browser side and metamask is running.
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    // This is for the server side or metamask is not running.
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/10b22c7c02744f43b09d0494cbfb32e2'
+    )
+    
+    // Created our own provider through the use of infura.
+    web3 = new Web3(provider);
+}
 
 export default web3;
