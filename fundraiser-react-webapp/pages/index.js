58edf0cee5f81113b6ea4327d2c230fbba218c2f
zillirammy <pau.edwin@live.ca>
2019-11-19 04:57:02 -0800

    Changes to web3.js to show deployed kickstarters

--- fundraiser-react-webapp/pages/index.js
+++ fundraiser-react-webapp/pages/index.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import instance from '../ethereum/instance';
 
 class KickstarterIndex extends Component {
-    async componentDidMount() {
+    // Required to be static by Next.js
+    static async getInitialProps() {
         const instances = await instance.methods.getDeployedInstances().call();
 
-        console.log(instances);
+        return { instances };
     }
 
     // Need to define a render method for some jsx
     render() {
-        return <div>Kickstarter Instances</div>
+        return <div>{this.props.instances[0]}</div>
     }
 }
 
