ff1112c094e5f2047488b88a8b38c142b1d6fb96
zillirammy <pau.edwin@live.ca>
2019-11-20 22:25:37 -0800

    added layout components

--- fundraiser-react-webapp/pages/index.js
+++ fundraiser-react-webapp/pages/index.js
@@ -29,6 +29,7 @@ class KickstarterIndex extends Component {
                     <link rel="stylesheet" 
                     href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
 
+                    <h3>Active Fundraisers</h3>
                     {this.renderInstances()}
 
                     <Button content="Create New Fundraiser" 
