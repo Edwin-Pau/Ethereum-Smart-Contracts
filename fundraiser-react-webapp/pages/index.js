e5d617359fc525486a8ac772b5032f8d8ce5c090
zillirammy <pau.edwin@live.ca>
2019-11-20 22:44:15 -0800

    added header/footer components

--- fundraiser-react-webapp/pages/index.js
+++ fundraiser-react-webapp/pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import instance from '../ethereum/instance';
 import { Card, Button } from 'semantic-ui-react'
+import Layout from '../components/layout'
 
 class KickstarterIndex extends Component {
     // Required to be static by Next.js
@@ -25,7 +26,8 @@ class KickstarterIndex extends Component {
 
     // Need to define a render method for some jsx
     render() {
-        return <div>
+        return <Layout>
+                <div>
                     <link rel="stylesheet" 
                     href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
 
@@ -34,7 +36,8 @@ class KickstarterIndex extends Component {
 
                     <Button content="Create New Fundraiser" 
                     icon="add circle" primary={true} labelPosition="right" />
-               </div>
+                </div>
+               </Layout>
     }
 }
 
