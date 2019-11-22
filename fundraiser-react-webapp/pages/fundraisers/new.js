777408770bde60a84d0d621c6ec443c855061b5c
zillirammy <pau.edwin@live.ca>
2019-11-21 23:02:03 -0800

    new fundraiser components

--- fundraiser-react-webapp/pages/fundraisers/new.js
+++ fundraiser-react-webapp/pages/fundraisers/new.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import Layout from '../../components/layout'
+import { Form, Button } from 'semantic-ui-react'
 
 class FundraiserNew extends Component {
     render() {
         return (
             <Layout>
-                <h1>New Fundraiser!</h1>
+                <h3>Create a Fundraiser!</h3>
+
+                <Form>
+                    <Form.Field>
+                        <label>Minimum Contribution</label>
+                        <input />
+                    </Form.Field>
+                
+                    <Button primary>Create</Button>
+                </Form>
+
             </Layout>
         )
     }
