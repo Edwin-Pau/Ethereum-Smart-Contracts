27a7b8634c9d16be154295a8bfa583794577a5b1
zillirammy <pau.edwin@live.ca>
2019-11-21 23:59:35 -0800

    added error message

--- fundraiser-react-webapp/pages/fundraisers/new.js
+++ fundraiser-react-webapp/pages/fundraisers/new.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 import Layout from '../../components/layout'
-import { Form, Button, Input } from 'semantic-ui-react'
+import { Form, Button, Input, Message } from 'semantic-ui-react'
+import instance from '../../ethereum/instance'
+import web3 from '../../ethereum/web3'
 
 class FundraiserNew extends Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: ''
+    }
+
+    onSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            // Get our accounts from metamask
+            const accounts = await web3.eth.getAccounts();
+
+            await instance.methods
+                .createNewInstance(this.state.minimumContribution)
+                .send({
+                    from: accounts[0]
+                })
+        } catch (err) {
+            this.setState({ errorMessage: err.message })
+        }
     }
 
     render() {
@@ -12,16 +32,19 @@ class FundraiserNew extends Component {
             <Layout>
                 <h3>Create a Fundraiser!</h3>
 
-                <Form>
+                <Form error={this.state.errorMessage} onSubmit={this.onSubmit}>
                     <Form.Field>
                         <label>Enter Minimum Contribution Amount</label>
-                        <Input 
+                        <Input
+                            style={{ width: '20vw' }} 
                             label="wei" 
                             labelPosition="right"
                             value={this.state.minimumContribution}
                             onChange={event => this.setState({ minimumContribution: event.target.value })} 
                         />
                     </Form.Field>
+
+                    <Message error header="Something went wrong!" content={this.state.errorMessage} />
                 
                     <Button primary>Create</Button>
                 </Form>
