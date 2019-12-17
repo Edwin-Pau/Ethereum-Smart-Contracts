4633a4cef4193be1f121e0aba3e80d77b74809de
Edwin-Pau <pau.edwin@live.ca>
2019-12-17 02:30:58 -0700

    display contract page layout

---
+++ fundraiser-react-dApp/pages/fundraisers/display.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Layout from '../../components/layout'
+
+class FundraiserDisplay extends Component {
+    render() {
+        return (
+            <Layout>
+                <h3>Display Fundraisers</h3>
+            </Layout>
+        )
+    }
+}
+
+export default FundraiserDisplay
--- fundraiser-react-dApp/routes.js
+++ fundraiser-react-dApp/routes.js
@@ -1,5 +1,8 @@
 const routes = require('next-routes')()
 
+// Default link for new fundraisers page.
+routes.add('fundraisers/new', '/fundraisers/new')
+
 // Add a wildcard URL to handle viewing existing fundraisers
 // If the user goes to the first argument, show the second argument.
 routes.add('/fundraisers/:address', '/fundraisers/display')
